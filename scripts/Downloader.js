4577c3b72ef48d3df825a59eeb3f381dc8ebc170
DragonDC1 <83457170+DragonDC1@users.noreply.github.com>
2021-06-01 10:58:51 +0200

    Add files via upload

---
+++ scripts/Downloader.js
@@ -0,0 +1,9 @@
+function DownloadFile() {
+var currentItem = document.getElementById('versions');
+
+
+if(currentItem.value == "1.7") document.getElementById('1.7').click();
+}
+
+var button = document.getElementById('download');
+button.addEventListener('click', "1.7", true);
